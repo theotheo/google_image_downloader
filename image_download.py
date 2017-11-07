2c4b03fc024afe962d4d3f3bad219c66f26d546f
Wayde Gilliam <wgilliam@ohmeow.com>
2017-11-07 09:51:13 -0800

    cleaned up, using pyton3.6 string interpolation where possible

--- image_download.py
+++ image_download.py
@@ -1,11 +1,7 @@
+import os, sys, time
+import json, requests, shutil
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import os
-import json
-import requests
-import shutil
-import sys
-import time
 
 # adding path to geckodriver to the OS environment variable
 os.environ["PATH"] += os.pathsep + os.getcwd()
@@ -20,13 +16,13 @@ def main():
 	if not os.path.exists(download_path + searchtext.replace(" ", "_")):
 		os.makedirs(download_path + searchtext.replace(" ", "_"))
 
-	url = "https://www.google.co.in/search?q="+searchtext+"&source=lnms&tbm=isch"
+	url = f'https://www.google.co.in/search?q={searchtext}&source=lnms&tbm=isch'
 	driver = webdriver.Firefox()
 	driver.get(url)
 
 	headers = {}
 	headers['User-Agent'] = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
-	extensions = {"jpg", "jpeg", "png", "gif"}
+	extensions = { "jpg", "jpeg", "png", "gif" }
 	img_count = 0
 	downloaded_img_count = 0
 	
@@ -37,39 +33,44 @@ def main():
 			time.sleep(0.2)
 		# to load next 400 images
 		time.sleep(0.5)
+
 		try:
 			driver.find_element_by_xpath("//input[@value='Show more results']").click()
+
 		except Exception as e:
-			print ("Less images found:", e)
+			print (f'Less images found: {e}')
 			break
 
-	# imges = driver.find_elements_by_xpath('//div[@class="rg_meta"]') # not working anymore
 	imges = driver.find_elements_by_xpath('//div[contains(@class,"rg_meta")]')
 	print ("Total images:", len(imges), "\n")
+
 	for img in imges:
+
 		img_count += 1
 		img_url = json.loads(img.get_attribute('innerHTML'))["ou"]
 		img_type = json.loads(img.get_attribute('innerHTML'))["ity"]
-		print ("Downloading image", img_count, ": ", img_url)
+		print (f'Downloading image {img_count} : {img_url}')
+
 		try:
-			if img_type not in extensions:
-				img_type = "jpg"
+			if img_type not in extensions: img_type = "jpg"
 
 			r = requests.get(img_url, stream=True, headers=headers)
 			if r.status_code == 200:
-				with open(download_path+searchtext.replace(" ", "_")+"/"+str(downloaded_img_count)+"."+img_type, "wb") as f:
+				with open(download_path + searchtext.replace(" ", "_") + "/" + str(downloaded_img_count) + "." + img_type, "wb") as f:
 					r.raw.decode_content = True
 					shutil.copyfileobj(r.raw, f)
-					
-			downloaded_img_count += 1
+					downloaded_img_count += 1
+
 		except Exception as e:
-			print ("Download failed:", e)
+			print (f'Download failed: {e}')
+
 		finally:
 			print('')
+
 		if downloaded_img_count >= num_requested:
 			break
 
-	print ("Total downloaded: ", downloaded_img_count, "/", img_count)
+	print (f'Total downloaded: {downloaded_img_count}/{img_count}')
 	driver.quit()
 
 if __name__ == "__main__":
